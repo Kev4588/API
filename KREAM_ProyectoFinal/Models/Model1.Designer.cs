a06b73bf2a61ce463fabbde6f63ef14e13b5c455
Emiliano Steinvorth <75647452+Steinvorth@users.noreply.github.com>
2024-08-12 23:01:58 -0600

    Connection

--- KREAM_ProyectoFinal/Models/Model1.Designer.cs
+++ KREAM_ProyectoFinal/Models/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\danie\OneDrive\Desktop\Proyecto Servicios Web\API\KREAM_ProyectoFinal\Models\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\ronal\OneDrive\Documents\GitHub\API\KREAM_ProyectoFinal\Models\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
